e6fafdeaa12736494baae336a2139a71eb80addf
Jake <yucj@dxy.cn>
2017-06-24 15:56:19 +0800

    切换yeoman api 到1.X

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,35 +2,36 @@ var path = require('path');
 var chalk = require('chalk'); // 不同颜色的info
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay'); // Yeoman弹出框
-var Reactpackage = yeoman.Base.extend({
-  info: function () {
+const fs = require('fs');
+module.exports = yeoman.extend({
+  info: function() {
     this.log(chalk.green(
       'I am going to build your front templates!'
     ));
   },
-  generateBasic: function () { // 按照自己的templates目录自定义
-    this.directory('src', 'src'); // 拷贝目录
-    this.directory('dist', 'dist');
-    this.copy('package.json', 'package.json'); // 拷贝文件
-    this.copy('README.md', 'README.md');
-    this.copy('README_en.md', 'README_en.md');
-    this.copy('webpack.config.dev.js', 'webpack.config.dev.js');
-    this.copy('webpack.config.pro.js', 'webpack.config.pro.js');
-    this.copy('gulpfile.js', 'gulpfile.js');
+  generateBasic: function() { // 按照自己的templates目录自定义
+    // 拷贝目录
+    this.fs.copy(this.templatePath("src"), this.destinationPath("src"));
+    // this.fs.copy(this.templatePath("dist"), this.destinationPath("dist"));
+    // 拷贝文件
+    this.fs.copy(this.templatePath("package.json"), this.destinationPath("package.json"));
+    this.fs.copy(this.templatePath("README.md"), this.destinationPath("README.md"));
+    this.fs.copy(this.templatePath("webpack.config.dev.js"), this.destinationPath("webpack.config.dev.js"));
+    this.fs.copy(this.templatePath("webpack.config.pro.js"), this.destinationPath("webpack.config.pro.js"));
+    this.fs.copy(this.templatePath("gulpfile.js"), this.destinationPath("gulpfile.js"));
   },
-  generateClient: function () {
+  generateClient: function() {
     this.sourceRoot(path.join(__dirname, 'templates'));
     this.destinationPath('./');
   },
-  install: function () { // 安装依赖
+  install: function() { // 安装依赖
     this.installDependencies({
       skipInstall: this.options['skip-install']
     });
   },
-  end: function () {
+  end: function() {
     this.log(yosay(
       'Your front templates has been created successfully!'
     ));
   }
 });
-module.exports = Reactpackage;
