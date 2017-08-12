d713937e8fe9a903795c0f29885ff0595ff41514
Jake <e@jakeyu.top>
2017-08-12 14:43:46 +0800

    add .gitignore

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,11 +14,35 @@ module.exports = yeoman.extend({
     this.fs.copy(this.templatePath("src"), this.destinationPath("src"));
     // this.fs.copy(this.templatePath("dist"), this.destinationPath("dist"));
     // 拷贝文件
-    this.fs.copy(this.templatePath("package.json"), this.destinationPath("package.json"));
-    this.fs.copy(this.templatePath("README.md"), this.destinationPath("README.md"));
-    this.fs.copy(this.templatePath("webpack.config.dev.js"), this.destinationPath("webpack.config.dev.js"));
-    this.fs.copy(this.templatePath("webpack.config.pro.js"), this.destinationPath("webpack.config.pro.js"));
-    this.fs.copy(this.templatePath("gulpfile.js"), this.destinationPath("gulpfile.js"));
+    this.fs.copy(
+      this.templatePath("package.json"),
+      this.destinationPath("package.json")
+    );
+
+    this.fs.copy(
+      this.templatePath("README.md"),
+      this.destinationPath("README.md")
+    );
+
+    this.fs.copy(
+      this.templatePath("webpack.config.dev.js"),
+      this.destinationPath("webpack.config.dev.js")
+    );
+
+    this.fs.copy(
+      this.templatePath("webpack.config.pro.js"),
+      this.destinationPath("webpack.config.pro.js")
+    );
+
+    this.fs.copy(
+      this.templatePath("gulpfile.js"),
+      this.destinationPath("gulpfile.js")
+    );
+
+    this.fs.copy(
+      this.templatePath(".gitignore"),
+      this.destinationPath(".gitignore")
+    );
   },
   generateClient: function() {
     this.sourceRoot(path.join(__dirname, 'templates'));
