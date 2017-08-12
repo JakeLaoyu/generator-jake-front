97e53b784aed6c793d00b2f309bd78b5e1fc52e1
Jake <e@jakeyu.top>
2017-08-13 05:12:07 +0800

    set author in package.json

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,14 +9,56 @@ module.exports = yeoman.extend({
       'I am going to build your front templates!'
     ));
   },
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the bedazzling ' + chalk.red('generator-jake-front') + ' generator! By Jake'
+    ));
+
+    const prompts = [{
+      type: 'confirm',
+      name: 'someAnswer',
+      message: 'Would you like to enable this option?',
+      default: true
+    }, {
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname // Default to current folder name
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'description'
+    }, {
+      type: 'input',
+      name: 'author',
+      message: 'author',
+      default: this.user.git.name()
+    }, {
+      type: 'input',
+      name: 'email',
+      message: 'email',
+      default: this.user.git.email()
+    }];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  },
   generateBasic: function() { // 按照自己的templates目录自定义
     // 拷贝目录
     this.fs.copy(this.templatePath("src"), this.destinationPath("src"));
     // this.fs.copy(this.templatePath("dist"), this.destinationPath("dist"));
     // 拷贝文件
-    this.fs.copy(
+    this.fs.copyTpl(
       this.templatePath("package.json"),
-      this.destinationPath("package.json")
+      this.destinationPath("package.json"), {
+        name: this.props.name,
+        description: this.props.description,
+        author: this.props.author,
+        email: this.props.email,
+      }
     );
 
     this.fs.copy(
@@ -50,7 +92,9 @@ module.exports = yeoman.extend({
   },
   install: function() { // 安装依赖
     this.installDependencies({
-      skipInstall: this.options['skip-install']
+      npm: true,
+      bower: false,
+      yarn: false
     });
   },
   end: function() {
